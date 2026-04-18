be953105cce94ffeb4f340a2102085ca22dcb743
qky120183609 <3453729573@qq.com>
2026-04-19 01:09:50 +0800

    Update china_vs_us_comparison.py

--- For Presentation/china_vs_us_comparison.py
+++ For Presentation/china_vs_us_comparison.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 # 从 CSV 读取数据
-df_all = pd.read_csv("../daily_stats.csv", parse_dates=["stat_date"])
+df_all = pd.read_csv("daily_stats.csv", parse_dates=["stat_date"])
 
 plt.rcParams['font.sans-serif'] = ['DejaVu Sans']
 plt.rcParams['axes.unicode_minus'] = False
